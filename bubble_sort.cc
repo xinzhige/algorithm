f9952779b5a96ebdd8aa3a5c8d6e66a154af6ae5
Isaiah Xin <xinzhige8@gmail.com>
2015-01-04 21:08:49 -0800

    Update bubble_sort.cc

--- bubble_sort.cc
+++ bubble_sort.cc
@@ -1,17 +1,17 @@
 #include <iostream>
 
-void swap(int &a, int &b) {
+void Swap(int &a, int &b) {
   int temp = a;
   a = b;
   b = temp;
 }
 
-void bubbleSort(int a[], int length) {
+void BubbleSort(int a[], int length) {
   int i, j;
   for (i = length - 1; i > 0; i--) {
     for (j = 0; j < i; j++) {
       if (a[j] > a[j+1]) {
-	      swap(a[j], a[j+1]);
+	      Swap(a[j], a[j+1]);
       }
     }
   }
@@ -19,7 +19,7 @@ void bubbleSort(int a[], int length) {
 
 int main(int args, char *argv[]) {
   int arr[] = {5, 4, 1, 3, 6};
-  bubbleSort(arr, 5);
+  BubbleSort(arr, 5);
   for(int i = 0; i < 5; i++) {
     std::cout<<arr[i]<<" ";
   }
